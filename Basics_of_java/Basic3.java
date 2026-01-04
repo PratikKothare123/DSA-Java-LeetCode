230213ac7c6f991a30d7871d3c716598b71377c1
Pratik Kothare <kotharepratik129@gmail.com>
2026-01-04 22:58:06 +0530

    Practice basic Java programs: addition, multiplication, circle area

---
+++ Basics_of_java/Basic3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Basic3 {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // taking Input of Two Numbers
+
+        // int n1= sc.nextInt();
+        // int n2 = sc.nextInt();
+
+
+        //Addition of two Number
+        // int n3 = n1+n2;
+        //   System.out.println("Addition Is: "+ n3 );
+
+        // Multiplication of Two Numbers
+        // int product = n1 * n2;
+        // System.out.println("Multiplication:"+product);
+
+        //Area Of Square 
+        // double radius = sc.nextDouble();
+        // double area = 3.14 * radius * radius ;
+        // System.out.println(area);
+
+        
+}
+}
+
