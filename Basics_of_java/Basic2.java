d5ca9b07527327d0746d9ccb4b4c7a8139965023
Pratik Kothare <kotharepratik129@gmail.com>
2026-01-04 22:24:26 +0530

    practice how to takr input in java

---
+++ Basics_of_java/Basic2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Basic2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // String name = sc.next();
+        // System.out.println(name);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int num = sc.nextInt();
+        // System.out.println(num);
+
+        // float num = sc.nextFloat();
+        // System.out.println(num);
+
+        // boolean ans = sc.nextBoolean();
+        // System.out.println(ans);  //true
+
+        // short ok = sc.nextShort();
+        // System.out.println(ok);
+
+
+        // long num = sc.nextLong();
+        // System.out.println(num);  //4565676576876
+
+    }
+}
