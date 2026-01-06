126f24f63beda9bcff81e07fa0899eb3c3c11f21
Pratik Kothare <kotharepratik129@gmail.com>
2026-01-07 00:24:54 +0530

    Practice Java arithmetic, relational, and logical operators

---
+++ Basics_of_java/Operators.java
@@ -0,0 +1,70 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        // Binarry Operators
+        // int A = 10;
+        // int B = 5;
+        // System.out.println("Addition:" + (A + B));
+        // System.out.println("Substraction:" + (A - B));
+        // System.out.println("Multiplication:" + (A * B));
+        // System.out.println("Division:" + (A / B));
+        // System.out.println("Modulio:" + (A % B));
+
+        // unary operators
+
+        // int a = 10; // Value change then Use
+        // int b = ++a;
+        // System.out.println(a); //11
+        // System.out.println(b); //11
+
+        // int a = 10; // value use then Change
+        // int b = a++;
+        // System.out.println(a); //11
+        // System.out.println(b); //10
+
+        // int a = 10;
+        // int b = --a;
+        // System.out.println(a); //9
+        // System.out.println(b); //9
+
+        // int a = 10;
+        // int b = a--;
+        // System.out.println(a); //9
+        // System.out.println(b); //10
+
+        // Relational operators
+
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a == b); // false
+        // System.out.println(a != b); // true
+        // System.out.println(a > b); //true
+        // System.out.println(a < b); //false
+        // System.out.println(a >= b); //true
+        // System.out.println(a <= b); //false
+
+        // //Logical Operators
+
+        // System.out.println((5>2) && (40<90)); // Both statment are ture So Result
+        // will be true, otherwise false
+        // System.out.println((3>8) || (5<1)); // Both statment are false So Result will
+        // be false, otherwise true
+        // System.out.println(!(5>8));
+
+        // Asigment Operators
+
+        // int A = 10;
+        // A= A + 10; //
+        // A += 10;
+        // System.out.println(A); //20
+
+        // int A = 10;
+        // // A = A - 10;
+        // A -= 10;
+        // System.out.println(A); //0
+
+
+        
+
+    }
+}
