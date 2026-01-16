3e1da2101e2c47c1996445742c7ceb3ad2d701c1
Pratik Kothare <kotharepratik129@gmail.com>
2026-01-16 23:29:33 +0530

    Patterns Problem Basic In Java

---
+++ Patterns/Patterns1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Patterns1 {
+    public static void main(String[] args) {
+        // int i = 1, j = 1;
+        // for (i = 1; i < 6; i++) {
+        // for (j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+
+        // }
+        // // *
+        // * *
+        // * * *
+        // * * * *
+        // * * * * *
+
+
+
+        
+        // int i = 1, j = 1;
+        // for (i=1;i<=5; i++) {
+        // for (j = 5; j >= i; j--) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+
+        // }
+        // * * * * *
+        // * * * *
+        // * * *
+        // * *
+        // *
+
+    }
+}
