46c99eebc1c0379fcc3be91989739b24d9f8c0ad
Pratik Kothare <kotharepratik129@gmail.com>
2026-01-17 23:42:56 +0530

    Patterns Questions

--- Patterns/Patterns1.java
+++ Patterns/Patterns1.java
@@ -16,9 +16,6 @@ public class Patterns1 {
         // * * * *
         // * * * * *
 
-
-
-        
         // int i = 1, j = 1;
         // for (i=1;i<=5; i++) {
         // for (j = 5; j >= i; j--) {
@@ -33,5 +30,31 @@ public class Patterns1 {
         // * *
         // *
 
+        // print Half-Pyramid Pattern
+        // int i = 1, j = 1;
+        // int n = 5;
+        // for (i = 1; i <= n; i++) {
+        // for (j = 1; j <= i; j++) {
+        // System.out.print(" " + j);
+        // }
+        // System.out.println();
+        // }
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
+
+        int i = 1, j = 1;
+        char ch='A';
+        int n = 4;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                
+                System.out.print( ch);
+                ch  ++;
+            }
+            System.out.println();
+        }
     }
 }
