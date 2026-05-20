e6f3e5e22dfcdadb4195468bc1876a0f7a22a63d
Pratik Kothare <kotharepratik129@gmail.com>
2026-05-20 21:15:02 +0530

    Striver's A2Z pattern No-11

--- Striver's A2Z Sheet/Patterns/Pattern1.java
+++ Striver's A2Z Sheet/Patterns/Pattern1.java
@@ -151,13 +151,13 @@
 // public class Pattern1 {
 //      public static void PatterPrint(int n){
 //         for(int i=0;i<n;i++){
-//             for(int j=0;j<n-i-1;j++){
+//             for(int j=0;j<i;j++){
 //                 System.out.print(" ");
 //             }
-//             for(int j=0;j<2*i+1;j++){
+//             for(int j=0;j<2*n-(2*i+1);j++){
 //                 System.out.print("*");
 //             }
-//             for(int j=0;j<n-i-1;j++){
+//             for(int j=0;j<n;j++){
 //                 System.out.print(" ");
 //             }
 
@@ -167,4 +167,37 @@
 //      public static void main(String[] args) {
 //         PatterPrint(5);
 //      }
-// }                            
+// }     
+// *********     
+//  *******
+//   *****
+//    ***
+//     *                       
+
+// public class Pattern1 {
+//     public static void Pattern10(int n){
+//         for(int  i=1;i<=2*n-1;i++){
+//             int stars = i;
+//             if(i>n)
+//                 stars = 2*n -i;
+//             for(int j=1;j<=stars;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println("");
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Pattern10(5);
+//     }
+// }
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
