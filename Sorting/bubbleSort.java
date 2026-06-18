3eda685feaef15c30fa22a133ced8531203b9a84
Pratik_Kothare <kotharepratik129@gmail.com>
2026-06-18 23:54:50 +0530

    Solved Bubble Sort

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -7,7 +7,7 @@ public class bubbleSort {
                 if(arr[j]>arr[j+1]){
                    int temp = arr[j];
                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j+1] = temp; // swap
 
                 }
             }
