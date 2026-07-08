f0f886277d79c9ff2a35091d92abc53fae889b3b
Pratik_Kothare <kotharepratik129@gmail.com>
2026-07-08 06:56:13 +0530

    solved Numberof1Bits Problem: Leetcode-191

---
+++ BitManupulation/Numberof1Bits.java
@@ -0,0 +1,18 @@
+package BitManupulation;
+
+public class Numberof1Bits {
+     public static int hammingWeight(int n) {
+        int count =0;
+        while(n>0){
+            if((n&1)!=0){
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int n=20;
+        System.out.println("Count: "+hammingWeight(n));
+    }
+}
