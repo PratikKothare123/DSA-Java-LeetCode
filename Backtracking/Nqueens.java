897156f270d3e5e8fd31affdc69298b7e84b0d5f
Pratik_Kothare <kotharepratik129@gmail.com>
2026-08-23 13:05:54 +0530

    Done, N-Queen Problem All Ways

--- Backtracking/Nqueens.java
+++ Backtracking/Nqueens.java
@@ -6,18 +6,55 @@ public class Nqueens {
 
         if(row==board.length){
             printBoard(board);
-            return board;
+            return ;
         }
-
-        for(int j=0;j<row;j++){
+        //Column
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board, row, j)){
             board[row][j] = 'Q';
             nQueens(board, row+1);
             board[row][j] = 'X';
+            }
+            
+        }
+    }
+    public static void printBoard(char board[][]){
+        System.out.println("---------Chess Bord-----------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static boolean isSafe(char board[][],int row,int col){
+        //Vertical Up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
         }
 
+        // Diagonal Left-Up
+        for(int i=row-1, j=col-1; i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+
+
+        // Diagonal Right-Up
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
     }
+
     public static void main(String[] args) {
-        int n = 2;
+        int n = 4;
         char board[][] = new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -26,6 +63,6 @@ public class Nqueens {
         }
 
         nQueens(board,0);
-        printBoard()
+        
     }
 }
