3ba9cb87c981b0b307727fe29a1b707e2263b378
Pratik_Kothare <kotharepratik129@gmail.com>
2026-09-01 20:59:49 +0530

    Start ArrayLists: Intro

---
+++ ArrayLists/Introduction.java
@@ -0,0 +1,10 @@
+package ArrayLists;
+import java.util.ArrayList;
+
+// ArrayList is a resizable array provided by Java's Collection Framework. 
+// // Unlike a normal array, its size can grow or shrink dynamically
+public class Introduction {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
